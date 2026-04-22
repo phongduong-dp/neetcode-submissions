6df60cba5523db85205bf263d9c7a6d5905b6fb7
Phong Duong <92157056+phongduong-dp@users.noreply.github.com>
2026-04-22 13:14:34 +0700

    Add: anagram-groups - submission-5

---
+++ Data Structures & Algorithms/anagram-groups/submission-5.cs
@@ -0,0 +1,21 @@
+public class Solution {
+    public List<List<string>> GroupAnagrams(string[] strs) {
+        var res = new Dictionary<string, List<string>>();
+
+        foreach(var s in strs){
+            var count = new int[26];
+
+            foreach (var c in s){
+                count[c - 'a']++;
+            }
+
+            var key = string.Join(",", count);
+            if (!res.ContainsKey(key)){
+                res[key] = new List<string>();
+            }
+            res[key].Add(s);
+        }
+
+        return res.Values.ToList<List<string>>();
+    }
+}
